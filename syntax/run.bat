a63072700cd7e1cc98ba97742f926fc16ea61915
Nicolai Drobyshevski <n.drobyshevski@pm.me>
2025-03-17 14:27:11 +0100

    Syntax: Clean up

--- syntax/run.bat
+++ syntax/run.bat
@@ -1,5 +1,4 @@
-
 @echo off
-gcc -o syntax syntax.c 
+gcc -o syntax syntax.c
 if %errorlevel% neq 0 exit /b %errorlevel%
-syntax
+syntax %*
--- syntax/test.bat
+++
@@ -1,6 +0,0 @@
-:: filepath: /c:/Users/nearl/projects/sdl_test/build_and_run.bat
-@echo off
-gcc -o syntax syntax.c 
-gcc -o test test.c 
-if %errorlevel% neq 0 exit /b %errorlevel%
-test
